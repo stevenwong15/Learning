cbe4975ba49af4b5edc37f4935e94b6cd5550644
Steven Wong <swong@cyence.net>
2019-02-01 17:07:20 -0800

    DCP L11, part 4

--- Python/udacity_4_bridge_successors.py
+++ Python/udacity_4_bridge_successors.py
@@ -1,22 +1,28 @@
-# -----------------
-# User Instructions
-# 
-# Write a function, bsuccessors(state), that takes a state as input
-# and returns a dictionary of {state:action} pairs.
-#
-# A state is a (here, there, t) tuple, where here and there are 
-# frozensets of people (indicated by their times), and potentially
-# the 'light,' t is a number indicating the elapsed time.
-#
-# An action is a tuple (person1, person2, arrow), where arrow is 
-# '->' for here to there or '<-' for there to here. When only one 
-# person crosses, person2 will be the same as person one, so the
-# action (2, 2, '->') means that the person with a travel time of
-# 2 crossed from here to there alone.
+def bridge_problem(here):
+    """
+    State will be a (people-here, people-there, time-elapsed)
+    """
+    here = frozenset(here) | frozenset(['light'])
+    explored = set() # set of states we have visited
+    frontier = [[(here, frozenset(), 0)]] # ordered list of paths we have blazed
+    if not here:
+        return frontier[0]
+    while frontier:
+        path = frontier.pop(0)
+        for (state, action) in bsuccessors(path[-1]).items():
+            if state not in explored:
+                here, there, t = state
+                explored.add(state)
+                path2 = path + [action, state]
+                if not here:  ## That is, nobody left here
+                    return path2
+                else:
+                    frontier.append(path2)
+                    frontier.sort(key=elapsed_time)
+    return []
 
-# frozenset(): immutable set, so can be used as key in dict
-
-import itertools
+def elapsed_time(path):
+    return path[-1][2]
 
 # dict comprehension
 def bsuccessors(state):
@@ -28,31 +34,35 @@ def bsuccessors(state):
     '<-' for there to here.
     """
     here, there, t = state
-
-
-    (*itertools.combinations(a - {"light"}, 2), "light")
-
-
-    dict((, , ): 
-        (a, b, "->")
-        for a in here
-        for b in here)
-
-
     if "light" in here:
-
-
+        return {
+            (here - frozenset([a, b, "light"]), 
+             there | frozenset([a, b, "light"]), 
+             t + max(a, b)): 
+            (a, b, "->")
+            for a in here if a is not "light"
+            for b in here if b is not "light"}
     else:
+        return {
+            (there | frozenset([a, b, "light"]), 
+             here - frozenset([a, b, "light"]), 
+             t + max(a, b)): 
+            (a, b, "<-")
+            for a in there if a is not "light"
+            for b in there if b is not "light"}
 
+# intead of list comprehension with condition, slice
+def path_states(path):
+    "Return a list of states in this path."
+    return path[0::2]
+
+def path_actions(path):
+    "Return a list of actions in this path."
+    return path[1::2]
 
 def test():
-
-    assert bsuccessors((frozenset([1, 'light']), frozenset([]), 3)) == {
-        (frozenset([]), frozenset([1, 'light']), 4): (1, 1, '->')}
-
-    assert bsuccessors((frozenset([]), frozenset([2, 'light']), 0)) == {
-        (frozenset([2, 'light']), frozenset([]), 2): (2, 2, '<-')}
-    
+    assert bridge_problem(frozenset((1, 2),))[-1][-1] == 2 # the [-1][-1] grabs the total elapsed time
+    assert bridge_problem(frozenset((1, 2, 5, 10),))[-1][-1] == 17
     return 'tests pass'
 
-print test()
+test()
