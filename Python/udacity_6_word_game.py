df0b127611a338f70e7497d286af7dca21755a3b
Steven Wong <swong@cyence.net>
2019-02-10 22:32:16 -0800

    DCP L18 27

--- Python/udacity_6_word_game.py
+++ Python/udacity_6_word_game.py
@@ -21,8 +21,8 @@ def readwordlist(filename):
 WORDS, PREFIXES = readwordlist("words4k.txt")
 
 POINTS = dict(
-	A=1, B=3, C=3, D=2, E=1, F=4, G=2, H=4, I=1, J=8, K=5, L=1, M=3, N=1, 
-	O=1, P=3, Q=10, R=1, S=1, T=1, U=1, V=4, W=4, X=8, Y=4, Z=10, _=0)
+    A=1, B=3, C=3, D=2, E=1, F=4, G=2, H=4, I=1, J=8, K=5, L=1, M=3, N=1, 
+    O=1, P=3, Q=10, R=1, S=1, T=1, U=1, V=4, W=4, X=8, Y=4, Z=10, _=0)
 
 #------------------------------------------------------------------------------
 # find words from hand
@@ -38,7 +38,7 @@ def find_words(letters, pre="", results=None):
     return results
 
 #------------------------------------------------------------------------------
-# find words from hand + board
+# find words from hand + letters on a board
 
 # Find prefix + L + suffix, where L from board_letters, rest from hand
 def word_plays(hand, board_letters):
@@ -96,6 +96,202 @@ def topn(hand, board_letters, n=10):
     Return a list of the top n words that hand can play, sorted by word score.
     """
     return sorted(word_plays(hand, board_letters), key=word_score, reverse=True)[:n]
+
+#------------------------------------------------------------------------------
+# find words from hand + row, considering constraints
+
+class anchor(set):
+    """
+    An anchor is where a new word can be placed; has a set of allowable letters
+    Left of anchor: prefix
+    Right of anchor: suffix
+    """
+
+LETTERS = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+ANY = anchor(LETTERS)  # an anchor that can be any letter
+
+# example: |A.....BE.C...D.|
+# using "|" to keep track of which spaces/letters are end of the board
+mnx, moab = anchor("MNX"), anchor("MOAB")  # anchors that are more restricted
+a_row = ["|", "A", mnx, moab, ".", ".", ANY, "B", "E", 
+    ANY, "C", ANY, ".", ANY, ".", ANY, "D", ANY, "|"]
+
+def is_letter(sq):
+    return isinstance(sq, str) and sq in LETTERS
+
+def is_empty(sq):
+    """
+    Is this an empty square (no letters, but a valid position on board).
+    """
+    return sq  == '.' or sq == '*' or isinstance(sq, set) 
+
+# plays a row:
+# for each square (sq) in the row, if anchor:
+# find all legal prefixes:
+# 1) if there are existing prefixes, add suffixes
+# 2) else, for all available prefixes that fits the length, add suffixes
+def row_plays(hand, row):
+    """
+    Return a set of legal plays in a row. A row play is an (i, "WORD") pair
+    """
+    for (i, sq) in enumerate(row[1:-1], 1):
+        if isinstance(sq, anchor):
+            pre, maxsize = legal_prefix(i, row)
+            if pre: 
+                start = i - len(pre)
+                add_suffixes(hand, pre, start, row, results, 
+                    anchored=False)
+            else:
+                for pre in find_prefixes(hand):
+                    if len(pre) <= maxsize:
+                        start = i - len(pre)
+                        add_suffixes(removed(hand, pre), pre, start, row, results, 
+                            anchored=False)
+    return results
+
+# example: legal_prefix(9, a_row) == ('BE', 2); for 6, ('', 2)
+# move left from anchor, till the first non-letter
+# if there is 1/+ letters: return (letters, spaces)
+# else, move left, till empty and not an anchor: return ("", spaces)
+def legal_prefix(i, row):
+    """
+    A legal prefix of an anchor at row[i] is either a string of letters
+    already on the board, or new letters that fit into an empty space.
+    Return the tuple (prefix_on_board, maxsize) to indicate this.
+    """
+    s = i
+    while is_letter(row[s-1]): s -= 1 
+    if s < i: return (''.join(row[s:i]), i-s)
+    while is_empty(row[s-1]) and not isinstance(row[s-1], set): s -= 1
+    return ('', i-s)
+
+# anchored=True means added anchor (i.e. not just letters on the board)
+# if pre is a word, anchor included and no letter to the right, include pre
+# else if pre is a prefix:
+# * if there's a letter at i, add the letter
+# * else go through possibilities
+def add_suffixes(hand, pre, start, row, results, anchored=True):
+    """
+    Add all possible suffixes, and accumulate (start, word) pairs in results
+    """
+    i = start + len(pre)
+    if pre in WORDS and anchored and not is_letter(row[i]):
+        results.add((start, pre))
+    if pre in PREFIXES:       
+        sq = row[i]
+        if is_letter(sq):
+            add_suffixes(hand, pre+sq, start, row, results)        
+        elif is_empty(sq):        
+            possibilities = sq if isinstance(sq, set) else ANY
+            for L in hand:
+                if L in possibilities:
+                    add_suffixes(hand.replace(L, '', 1), pre+L, start, row, results)
+    return results
+
+# in row_plays(), find_prefixes() gets called in every loop, which is slow
+# so, build a cache for find_prefixes(), and set it as global variables
+# find_prefixes() returns both prefixes and words, since:
+# * the "pivot" between pre/suff is an anchor, not letter
+# * thus, you can go without suffix to form a word
+prev_hand, prev_results = "", set()
+def find_prefixes(hand, pre="", results=None):
+    global prev_hand, prev_results
+    if hand == prev_hand: return prev_results
+    if results is None: results = set()
+    # at the root of recursion, cache results
+    if pre == "": prev_hand, prev_results = hand, results
+    if pre in WORDS or pre in PREFIXES: results.add(pre)
+    if pre in PREFIXES:
+        for L in hand:
+            find_prefixes(hand.replace(L, "", 1), pre+L, results)
+    return results
+
+#------------------------------------------------------------------------------
+# find words from hand + board, considering constraints
+
+def neighbors(board, i, j):
+    """
+    Return the four neighboring squares, in the order N,S,E,W
+    """
+    return [board[j-1][i], board[j+1][i],
+            board[j][i+1], board[j][i-1]]
+
+def find_cross_word(board, i, j):
+    """
+    Find the vertical word that crosses board[j][i]; return (starting row, word)
+    """
+    sq = board[j][i]
+    w = sq if is_letter(sq) else '.'
+    # from j, go up
+    for j2 in range(j, 0, -1):
+        sq2 = board[j2-1][i]
+        if is_letter(sq2): w = sq2 + w
+        else: break
+    # from j, go down
+    for j3 in range(j+1, len(board)):
+        sq3 = board[j3][i]
+        if is_letter(sq3): w = w + sq3
+        else: break
+    return (j2, w)
+
+def set_anchors(row, j, board):
+    """
+    Adjust the board to include anchors
+    Anchors are empty squares with a neighboring letter or "*"
+    Some are resticted by cross-words to be only a subset of letters.
+    """
+    for (i, sq) in enumerate(row[1:-1], 1):
+        neighborlist = (N,S,E,W) = neighbors(board, i, j)
+        if sq == '*' or (is_empty(sq) and any(map(is_letter, neighborlist))):    
+            if is_letter(N) or is_letter(S):   
+                _, w = find_cross_word(board, i, j)
+                row[i] = anchor(L for L in LETTERS if w.replace('.', L) in WORDS)
+            else:
+                row[i] = ANY
+
+def horizontal_plays(hand, board):
+    """
+    Find all horizontal plays -- ((i, j), word) pairs -- across all rows.
+    """
+    results = set()
+    for (j, row) in enumerate(board[1:-1], 1):
+        set_anchors(row, j, board)
+        for i, word in row_plays(hand, row):
+            results.add(((i, j), word))
+    return results
+
+def transpose(matrix):
+    return map(list, zip(*matrix))
+
+ACROSS, DOWN = (1, 0), (0, 1) # Directions that words can go
+
+def all_plays(hand, board):
+    """
+    All plays in both directions. A play is a (pos, dir, word) tuple,
+    where pos is an (i, j) pair, and dir is ACROSS or DOWN.
+    """
+    hplays = horizontal_plays(hand, board)            # set of ((i, j), word)
+    vplays = horizontal_plays(hand, transpose(board)) # set of ((j, i), word)
+    return (
+        {((i, j), ACROSS, word) for ((i, j), word) in hplays} |
+        {((i, j), DOWN, word) for ((j, i), word) in vplays})
+
+#------------------------------------------------------------------------------
+# printing a board
+    
+def a_board():
+    return map(list, ['|||||||||||||||||',
+                      '|J............I.|',
+                      '|A.....BE.C...D.|',
+                      '|GUY....F.H...L.|',
+                      '|||||||||||||||||'])
+
+def show(board):
+    """Print the board."""
+    for i in board:
+        print(" ".join(i)) 
+               
+show(a_board())
 
 #------------------------------------------------------------------------------
 # tests
