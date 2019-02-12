d1103a7060d04c9db0e700b4603086a368d3ae49
Steven Wong <swong@cyence.net>
2019-02-11 18:06:17 -0800

    DCP L19 part 1

--- Python/udacity_6_anagrams.py
+++ Python/udacity_6_anagrams.py
@@ -1,35 +1,47 @@
-# -----------------
-# User Instructions
-# 
-# This homework deals with anagrams. An anagram is a rearrangement 
-# of the letters in a word to form one or more new words. 
-#
-# Your job is to write a function anagrams(), which takes as input 
-# a phrase and an optional argument, shortest, which is an integer 
-# that specifies the shortest acceptable word. Your function should
-# return a set of all the possible combinations of anagrams. 
-#
-# Your function should not return every permutation of a multi word
-# anagram: only the permutation where the words are in alphabetical
-# order. For example, for the input string 'ANAGRAMS' the set that 
-# your function returns should include 'AN ARM SAG', but should NOT 
-# include 'ARM SAG AN', or 'SAG AN ARM', etc...
+#------------------------------------------------------------------------------
+# read dictionary
+
+def prefixes(word):
+    """
+    A list of the initial sequences of a word, not including the complete word.
+    """
+    return [word[:i] for i in range(len(word))]
+
+def readwordlist(filename):
+    """
+    Return a pair of sets: all the words in a file, and all the prefixes. (Uppercased.)
+    """
+    wordset = set(open(filename).read().upper().split())
+    prefixset = set(p for word in wordset for p in prefixes(word))
+    return wordset, prefixset
+
+WORDS, PREFIXES = readwordlist('words4k.txt')
+
+#------------------------------------------------------------------------------
+# read list
 
 def anagrams(phrase, shortest=2):
-    """Return a set of phrases with words from WORDS that form anagram
+    """
+    Return a set of phrases with words from WORDS that form anagram
     of phrase. Spaces can be anywhere in phrase or anagram. All words 
     have length >= shortest. Phrases in answer must have words in 
-    lexicographic order (not all permutations)."""
-    # your code here
-    
-# ------------
-# Helpful functions
-# 
-# You may find the following functions useful. These functions
-# are identical to those we defined in lecture. 
+    lexicographic order (not all permutations).
+    """
+    return find_anagrams("", phrase, set(), shortest)
+
+def find_anagrams(pre, phrase, results, shortest):
+    phrase = removed(phrase, pre)
+    if phrase == "": 
+        results.add(tuple(sorted(pre.split())))
+    for i in find_words(phrase):
+        if len(i) >= shortest:
+            find_anagrams(pre + " " + i, phrase, results, shortest)
+    return set([" ".join(i) for i in results])
 
 def removed(letters, remove):
-    "Return a str of letters, but with each letter in remove removed once."
+    """
+    Return a str of letters, but with each letter in remove removed once.
+    """
     for L in remove:
         letters = letters.replace(L, '', 1)
     return letters
@@ -41,25 +53,11 @@ def extend_prefix(pre, letters, results):
     if pre in WORDS: results.add(pre)
     if pre in PREFIXES:
         for L in letters:
-            extend_prefix(pre+L, letters.replace(L, '', 1), results)
+            extend_prefix(pre+L, removed(letters, L), results)
     return results
 
-def prefixes(word):
-    "A list of the initial sequences of a word, not including the complete word."
-    return [word[:i] for i in range(len(word))]
-
-def readwordlist(filename):
-    "Return a pair of sets: all the words in a file, and all the prefixes. (Uppercased.)"
-    wordset = set(open(filename).read().upper().split())
-    prefixset = set(p for word in wordset for p in prefixes(word))
-    return wordset, prefixset
-
-WORDS, PREFIXES = readwordlist('words4k.txt')
-
-# ------------
-# Testing
-# 
-# Run the function test() to see if your function behaves as expected.
+#------------------------------------------------------------------------------
+# test
 
 def test():
     assert 'DOCTOR WHO' in anagrams('TORCHWOOD')
@@ -73,5 +71,4 @@ def test():
         'CON HYP TI'])
     return 'tests pass'
 
-print test()
-
+test()
